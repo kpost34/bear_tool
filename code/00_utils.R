a075b8b9f658691b45672850ebc147a0de6e3de0
Keith <kpost342005@hotmail.com>
2026-01-03 09:37:22 -0500

    Moved re-usable functions to separate 00 script

---
+++ code/00_utils.R
@@ -0,0 +1,32 @@
+# Helper Functions Used Throughout Project
+
+## Function to dynamically combine elements into a string
+dynamic_paste <- function(vec, add_quotes=TRUE){
+  n <- length(vec)
+  if(add_quotes){
+    vec <- purrr::map_chr(vec, function(x) paste0("'", x, "'"))
+  }
+  vec_str <- if(n==1) {
+    vec
+  } else if(n==2){
+    paste(vec, collapse=" and ")
+  } else if(n>2){
+    paste(vec, collapse=", ") %>%
+      str_replace(", (?=[^,]*$)", " and ")
+  }
+  
+  return(vec_str)
+}
+
+
+## Function to stack messages without producing empty lines
+append_message <- function(msg, msg_new){
+  if(msg==""){
+    msg_new
+  } else{
+    paste(msg, msg_new, sep="\n")
+  }
+}
+
+
+
